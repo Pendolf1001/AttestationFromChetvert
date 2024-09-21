e2b2a23d4315e10aee226ce3d708042390a105c6
Evgeniy K <kovalev8915512@gmail.com>
2024-09-21 19:49:25 +0300

    Create class AnimalBuilder

--- SRC/ReestrOfAnimals/src/Model/AnimalBuilder.java
+++ SRC/ReestrOfAnimals/src/Model/AnimalBuilder.java
@@ -1,4 +1,30 @@
 package Model;
 
-public class AnimalBuilder {
+class AnimalBuilder < T extends Animals>  {
+    private T t;
+
+
+    public T animalBuild (String type, String name, String birthDate ){
+        if (type.equals("Dog")){
+            Dog s=new Dog(name,birthDate );
+            t=(T)s;
+        }else if(type.equals("Cat")){
+            Cat s=new Cat(name,birthDate );
+            t=(T)s;
+        }else if(type.equals("Hamster")){
+            Hamster s=new Hamster(name,birthDate );
+            t=(T)s;
+        }else if(type.equals("Horse")){
+            Horse s=new Horse(name,birthDate );
+            t=(T)s;
+        }else if(type.equals("Camal")){
+            Camal s=new Camal(name,birthDate );
+            t=(T)s;
+        }else if(type.equals("Donkey")){
+            Donkey s=new Donkey(name,birthDate );
+            t=(T)s;
+        }
+
+        return t;
+    }
 }
