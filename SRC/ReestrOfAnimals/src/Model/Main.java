d0bfe9e9a1c950ecb330f8c2d06b147db63576e5
Evgeniy K <kovalev8915512@gmail.com>
2024-09-21 19:29:06 +0300

    Create class Hamster

--- SRC/ReestrOfAnimals/src/Model/Main.java
+++ SRC/ReestrOfAnimals/src/Model/Main.java
@@ -3,7 +3,7 @@ package Model;
 public class Main {
     public static void main(String[] args) {
 
-        Cat a= new Cat("Asia", "2020.11.11");
+        Animals a= new Hamster("Asia", "2020.11.11");
 
 
         System.out.println(a);
