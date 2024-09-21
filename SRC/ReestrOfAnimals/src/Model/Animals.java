45db12ae684c30df68e37f74db0c044088ab21aa
Evgeniy K <kovalev8915512@gmail.com>
2024-09-21 18:55:39 +0300

    Create Pets

--- SRC/ReestrOfAnimals/src/Model/Animals.java
+++ SRC/ReestrOfAnimals/src/Model/Animals.java
@@ -1,4 +1,28 @@
 package Model;
 
-public class Animals {
+import java.time.*;
+import java.util.*;
+import java.time.format.DateTimeFormatter;
+
+
+abstract class Animals {
+    String name;
+    private LocalDate birthDate;
+    HashMap <String, String> commands;
+    private int id;
+    private int age;
+
+    abstract void addCommand(String key, String move);
+    abstract String getCommand();
+    abstract String execCommand(String key);
+    abstract String getName();
+    abstract String getYaerBith();
+
+
+    int getAge(){
+        return this.age;
+    }
+    LocalDate getBirbirthDate(){
+        return this.birthDate;
+    }
 }
---
+++ SRC/ReestrOfAnimals/src/Model/Pets.java
@@ -0,0 +1,14 @@
+package Model;
+
+abstract class Pets extends Animals{
+    // String name;
+    // private LocalDate birthDate;
+    // HashMap <String, String> commands;
+
+    // abstract void addCommand(String key, String move);
+    // abstract String getCommand();
+    // abstract String execCommand(String key);
+    abstract String vzyatNaRuchki();
+
+
+}
